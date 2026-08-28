d9a6007e8f3f9cf31a55c2bbf0fc39de84e08e91
Minhaz EsmAil <minhazesmail@gmail.com>
2026-08-29 01:17:50 +0600

    chore: allow signed property media images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "naoarepmcfdnxehbdios.supabase.co",
+        pathname: "/storage/v1/object/sign/property-media/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
