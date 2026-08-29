d562e9b2f6d28c17cc0ad7d2b63e8da433fcde3f
Minhaz EsmAil <minhazesmail@gmail.com>
2026-08-29 23:48:57 +0600

    feat: enable realtime message inserts

---
+++ supabase/migrations/20260829035000_enable_message_realtime.sql
@@ -0,0 +1,13 @@
+do $$
+begin
+  if not exists (
+    select 1
+    from pg_publication_tables
+    where pubname = 'supabase_realtime'
+      and schemaname = 'public'
+      and tablename = 'messages'
+  ) then
+    alter publication supabase_realtime add table public.messages;
+  end if;
+end
+$$;
