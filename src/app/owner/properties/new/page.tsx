0c4521694cfcec7fe6b5651fde33ea946f4808e3
Minhaz EsmAil <minhazesmail@gmail.com>
2026-09-03 23:08:01 +0600

    Load listing media preview styles

--- src/app/owner/properties/new/page.tsx
+++ src/app/owner/properties/new/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { PropertyListingForm } from "@/components/property-listing-form";
 import { requireOwnerOrAgent } from "@/lib/auth";
 import { createClient } from "@/lib/supabase/server";
+import "../listing-media-ux.css";
 export const dynamic = "force-dynamic";
 
 export default async function NewPropertyPage() {
