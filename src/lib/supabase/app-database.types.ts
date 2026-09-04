dc18da8adae9f7e36dd2d95d14c513878673b559
Minhaz EsmAil <minhazesmail@gmail.com>
2026-09-05 02:17:56 +0600

    fix: type audited Supabase RPCs

---
+++ src/lib/supabase/app-database.types.ts
@@ -0,0 +1,22 @@
+import type { Database } from "./database.types";
+
+type AuditFunctions = {
+  get_my_property_moderation_notes: {
+    Args: { property_ids: string[] };
+    Returns: { property_id: string; moderation_notes: string | null }[];
+  };
+  replace_property_listing_relations: {
+    Args: {
+      property_uuid: string;
+      tenant_values: Database["public"]["Enums"]["tenant_type"][];
+      amenity_values: string[];
+    };
+    Returns: undefined;
+  };
+};
+
+export type AppDatabase = Omit<Database, "public"> & {
+  public: Omit<Database["public"], "Functions"> & {
+    Functions: Database["public"]["Functions"] & AuditFunctions;
+  };
+};
