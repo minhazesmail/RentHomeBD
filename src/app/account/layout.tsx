f2bbe645481fae4447dbf59541cf6764d0d98852
Minhaz EsmAil <minhazesmail@gmail.com>
2026-09-04 17:36:40 +0600

    perf: scope account auth styles to account routes

---
+++ src/app/account/layout.tsx
@@ -0,0 +1,6 @@
+import "../auth.css";
+import "../auth-verification-redesign.css";
+
+export default function AccountLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return children;
+}
