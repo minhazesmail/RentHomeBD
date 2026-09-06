3abf5a839a81936e60222f69cb2cd266ccff1bb7
Minhaz EsmAil <minhazesmail@gmail.com>
2026-09-07 04:27:56 +0600

    feat: add theme context hook

---
+++ src/theme/use-theme.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { useContext } from "react";
+
+import { ThemeContext } from "./theme-provider";
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error("useTheme must be used inside ThemeProvider");
+  return context;
+}
